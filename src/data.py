e50cd6d34e29ca34bf464d4c6405cfb1cd7ec4be
realfolkcode <wtfomgrofl@mail.ru>
2023-10-30 12:54:51 +0300

    Calculate and plot cross-similarity

--- src/data.py
+++ src/data.py
@@ -1,5 +1,6 @@
 import pretty_midi
 import torch
+import torch.nn.functional as F
 import numpy as np
 
 
@@ -14,6 +15,25 @@ def extract_piano_roll(midi_path: str, fs: int = 40) -> torch.Tensor:
         A binary piano roll tensor of shape (num_frames, 128).
     """
     pm = pretty_midi.PrettyMIDI(midi_path)
-    piano_roll = (pm.get_piano_roll(fs) > 0).astype('float').T
+    piano_roll = (pm.get_piano_roll(fs) > 0).astype('float32').T
     piano_roll = torch.from_numpy(piano_roll)
     return piano_roll
+
+
+def calculate_cross_similarity(score_roll: torch.Tensor, 
+                               perf_roll: torch.Tensor,
+                               eps: float = 1e-6) -> torch.Tensor:
+    """Calculates cross-similarity matrix between score and performance.
+
+    Args:
+        score_roll: Score piano roll tensor of shape (score_frames, 128).
+        perf_roll: Performance piano roll tensor of shape (perf_frames, 128).
+        eps: Small value to avoid division by zero.
+
+    Returns:
+        A cross-similarity matrix of shape (score_frames, perf_frames).
+    """
+    normalized_score_roll = score_roll / (torch.norm(score_roll, dim=0) + eps)
+    normalized_perf_roll = perf_roll / (torch.norm(perf_roll, dim=0) + eps)
+    cross_similarity = torch.cdist(normalized_score_roll, normalized_perf_roll)
+    return cross_similarity
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,10 @@
 import librosa
 import pretty_midi
+import torch
+import matplotlib.pyplot as plt
 
 
-def plot_piano_roll(piano_roll, fs: int, start_pitch: int = 0, end_pitch: int = 128) -> None:
+def plot_piano_roll(piano_roll: torch.Tensor, fs: int, start_pitch: int = 0, end_pitch: int = 128) -> None:
     """Plots a piano roll representation.
 
     Args:
@@ -14,3 +16,18 @@ def plot_piano_roll(piano_roll, fs: int, start_pitch: int = 0, end_pitch: int = 
     librosa.display.specshow(piano_roll.T.numpy()[start_pitch:end_pitch + 1],
                              hop_length=1, sr=fs, x_axis='time', y_axis='cqt_note',
                              fmin=pretty_midi.note_number_to_hz(start_pitch))
+
+
+def plot_cross_similarity(cross_similarity: torch.Tensor) -> None:
+    """Plots a cross-similarity matrix.
+
+    Args:
+        cross_similarity: A cross-similarity matrix of shape 
+          (score_frames, perf_frames).
+    """
+    plt.figure(figsize=(10, 10))
+    plt.imshow(cross_similarity)
+    plt.title('Cross-similarity matrix')
+    plt.xlabel('Performance frames')
+    plt.ylabel('Score frames')
+    plt.colorbar()
