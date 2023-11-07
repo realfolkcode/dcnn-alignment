8aad903180ffb6acf814153b98bebf2855cf3c8f
realfolkcode <wtfomgrofl@mail.ru>
2023-11-07 12:44:03 +0300

    Add Dataset class

--- src/data.py
+++ src/data.py
@@ -1,10 +1,11 @@
-from typing import List
+from typing import List, Dict, Optional, Callable
 
 from .utils import seconds_to_frames
 
 import pretty_midi
 import torch
 import torch.nn.functional as F
+from torch.utils.data import Dataset
 import numpy as np
 
 
@@ -66,3 +67,58 @@ def construct_beat_alignment(perf_beats: np.ndarray,
 
     beat_alignment = np.vstack((perf_beats_f, score_beats_f))
     return beat_alignment
+
+
+class CrossSimilarityDataset(Dataset):
+    """Performance-score cross-similarity matrices dataset."""
+
+    def __init__(self,
+                 pairs: List[Dict],
+                 fs: int,
+                 transform: Callable,
+                 structural_transform: Optional[Callable] = None,
+                 inference_only: bool = False):
+        """Initializes an instance of dataset class.
+
+        Args:
+            pairs: A list of performance-score path pairs and (optionally) their aligned
+              beat arrays. Each dictionary in a list must contain keys `perf`, `score`.
+              If `structural_transform` is not None, then `perf_beats`, and `score_beats`
+              must also be keys.
+            fs: Piano roll sampling frequency.
+            transform: Transformation to apply to cross-similarity matrices (e.g., resize).
+            structural_transform: Structural transformations to apply to performance
+              piano rolls.
+            inference_only: If True, dataset does not contain ground truth inflection points.
+        """
+        self.pairs = pairs
+        self.fs = fs
+        self.transform = transform
+        self.structural_transform = structural_transform
+        self.inference_only = inference_only
+
+    def __len__(self):
+        return len(self.pairs)
+
+    def __getitem__(self, idx):
+        perf_path = self.pairs[idx]['perf']
+        score_path = self.pairs[idx]['score']
+        if self.structural_transform is not None:
+            perf_beats = np.array(self.pairs[idx]['perf_beats'])
+            score_beats = np.array(self.pairs[idx]['score_beats'])
+
+        perf_roll = extract_piano_roll(perf_path, fs=self.fs)
+        score_roll = extract_piano_roll(score_path, fs=self.fs)
+
+        if self.structural_transform is not None:
+            beat_alignment = construct_beat_alignment(perf_beats, score_beats, self.fs)
+            perf_roll, beat_alignment, inflection_points = self.structural_transform(perf_roll, score_roll, beat_alignment)
+
+        cross_similarity = calculate_cross_similarity(perf_roll, score_roll)
+        cross_similarity = self.transform(cross_similarity)
+        sample = {'image': cross_similarity}
+
+        if not self.inference_only:
+            sample['target'] = inflection_points
+
+        return sample
